b28a1252674c3ff784ae12b50ae26b32ff46a38e
fuadmn <fuadmohamednor@gmail.com>
2025-03-16 22:43:19 +0300

    first commit

--- 18 HTML and CSS Final Project Preview/2 Responsive Landing Page.js
+++
@@ -1,6 +0,0 @@
-let burger = document.querySelector(".burger");
-let navigationBar = document.querySelector(".navigation-bar");
-
-burger.addEventListener("click",  ()=> {
-navigationBar.classList.toggle('active');
-});
